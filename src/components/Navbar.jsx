696faee1b74aa4add2d8a176a3f71b3a15979c8e
Aria <Aria.Blackpurple@gmail.com>
2024-10-05 10:55:27 +0330

    dark

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,7 @@ import UserImg from "../assets/landing/user.png";
 import ResponsiveMenu from './ResponsiveMenu';
 import { MdDarkMode, MdMenu } from 'react-icons/md';
 import DarkMode from './DarkMode';
+import { Link } from 'react-router-dom';
 
 
 
@@ -55,12 +56,12 @@ const Navbar = () => {
 <div className='container flex justify-between items-center py-6 dark:bg-gray-900  duration-200 '>
  {/* ACCOUNT section  */}
  <div className='text-2xl flex items-center gap-2 font-bold'>
-    <div className='relative w-36 max-md:w-[150px]  max-sm:w-[150px] max-lg:w-[150px] max-xl:w-[200px]  h-10 bg-primary
+    <Link to="/auth" className='relative w-36 max-md:w-[150px]  max-sm:w-[150px] max-lg:w-[150px] max-xl:w-[200px]  h-10 bg-primary
      mt-8 rounded-3xl hover:shadow-lg hover:shadow-orange dark:bg-orange  dark:hover:shadow-lg dark:hover:shadow-orange
     transition-shadow'>
      <img src={UserImg} alt="" className='w-7 h-7  absolute top-1.5 left-4'/>
      <h1 className='text-sm  font-semibold text-white text-right leading-loose px-2 py-1 '>حساب کاربری</h1>
-  </div>  
+  </Link>  
   <div className='flex justify-center flex-row gap-4 ml-4'>
       <img src={ShopImg} alt="" className='mt-8'/>
       <div className='relative'>
