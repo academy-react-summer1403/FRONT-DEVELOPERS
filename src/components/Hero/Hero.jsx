611acdec674de85a545c21b94d934c6bb7f8245b
fatemeh_bkhshian <flowerfbf18@gmail.com>
2024-10-05 14:53:15 +0330

    commite

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -13,92 +13,93 @@ import HatImg from "../../assets/landing/Hat.png";
 
 
 
+
 const Hero = () => {
     return (
       <>
         <div
           className="container grid grid-cols-1 md:grid-cols-2 
-      min-h-[650px] relative"
+          min-h-[650px] relative"
         >
           <img src={BackImg} alt="" style={{backgroundPosition:"50%"}} className='w-full h-full absolute opacity-80'/>
             
 
-   {/* hero image  */}
-   <div className="flex justify-center items-center relative -mr-16">
+          {/* hero image  */}
+          <div className="flex justify-center items-center relative -mr-16">
 
 
-        <div className='flex justify-start  w-full h-[100px] absolute top-20 max-xl:right-[100px] max-xl:top-[20px] max-md:right-[60px]
-        max-sm:left-[20px]'>
-        <div className='w-[55px] h-[55px] rounded-full bg-yellow-500 absolute left-[83px] top-[-25px] z-50'>
-          <img src={TImg} alt="" className='z-40 px-2 py-2'/>
-        </div>
-            <div className='w-[180px] h-full ml-5 bg-white rounded-2xl shadow-2xl z-40 border border-gray-600/10
-            hover:shadow-xl hover:shadow-orange transition-shadow dark:bg-gray-700/70  dark:hover:bg-orange dark:hover:shadow-md dark:hover:transition-colors'>
-              <h1 className='text-lg text-center py-12 text-gray-800/90 font-semibold dark:text-white'> ۱۰۰۰+  دانشجو </h1>
-            </div>
-        </div>
+                <div className='flex justify-start  w-full h-[100px] absolute top-20 max-xl:right-[100px] max-xl:top-[20px] max-md:right-[60px]
+                max-sm:left-[20px]'>
+                <div className='w-[55px] h-[55px] rounded-full bg-yellow-500 absolute left-[83px] top-[-25px] z-50'>
+                  <img src={TImg} alt="" className='z-40 px-2 py-2'/>
+                </div>
+                    <div className='w-[180px] h-full ml-5 bg-white rounded-2xl shadow-2xl z-40 border border-gray-600/10
+                    hover:shadow-xl hover:shadow-orange transition-shadow dark:bg-gray-700/70  dark:hover:bg-orange dark:hover:shadow-md dark:hover:transition-colors'>
+                      <h1 className='text-lg text-center py-12 text-gray-800/90 font-semibold dark:text-white'> ۱۰۰۰+  دانشجو </h1>
+                    </div>
+                </div>
 
 
-   <motion.div
-              initial={{opacity:0 , x:200}}
-              animate={{opacity:1 , x:0}}
-              transition={{type:"spring" , stiffness:100 , delay:0.2}}
-  
-              className="w-[500px]  h-[450px]   max-xl:w-[500px] max-xl:h-[400px] 
-               rounded-2xl bg-gradient-to-r from-primary/20 to-light/10 z-20 max-xl:-mt-[60px] max-md:w-[400px] max-md:h-[400px]
-               max-md:mt-[50px] max-md:mr-[50px] dark:from-primary/50 dark:to-light/30"
-            >
+          <motion.div
+                      initial={{opacity:0 , x:200}}
+                      animate={{opacity:1 , x:0}}
+                      transition={{type:"spring" , stiffness:100 , delay:0.2}}
+          
+                      className="w-[500px]  h-[450px]   max-xl:w-[500px] max-xl:h-[400px] 
+                      rounded-2xl bg-gradient-to-r from-primary/20 to-light/10 z-20 max-xl:-mt-[60px] max-md:w-[400px] max-md:h-[400px]
+                      max-md:mt-[50px] max-md:mr-[50px] dark:from-primary/50 dark:to-light/30"
+                    >
 
 
-            <div>
-            <motion.img
-              initial={{opacity:0 , x:200}}
-              animate={{opacity:1 , x:0}}
-              transition={{type:"spring" , stiffness:100 , delay:0.2}}
-  
-              src={HeroAImg}
-              alt=""
-              className="w-[330px] h-[280px] xl:h-[380px] absolute left-[20px] top-[50px]
-              xl:w-[480px] max-xl:left-[100px] max-md:left-[30px] max-md:w-[700px] max-md:h-[330px] max-lg:left-[60px]"
-            />
+                    <div>
+                    <motion.img
+                      initial={{opacity:0 , x:200}}
+                      animate={{opacity:1 , x:0}}
+                      transition={{type:"spring" , stiffness:100 , delay:0.2}}
+          
+                      src={HeroAImg}
+                      alt=""
+                      className="w-[330px] h-[280px] xl:h-[380px] absolute left-[20px] top-[50px]
+                      xl:w-[480px] max-xl:left-[100px] max-md:left-[30px] max-md:w-[700px] max-md:h-[330px] max-lg:left-[60px]"
+                    />
+                  </div>
+
+
+            </motion.div>
+
+            <div className="flex justify-center items-center">
+                    <motion.img
+                      initial={{opacity:0 , x:200}}
+                      animate={{opacity:1 , x:0}}
+                      transition={{type:"spring" , stiffness:100 , delay:0.2}}
+          
+                      src={HeroBImg}
+                      alt=""
+                      className="w-[400px] md:w-[600px] absolute left-24 top-[270px]
+                      xl:w-[550px] max-xl:left-[0px] max-md:left-[50px] max-md:top-[180px] max-md:w-[500px]"
+                    />
+                  </div>
+
+                  <div className='flex justify-end  w-full h-[100px] absolute bottom-5 left-3 max-xl:mb-[30px] max-md:top-[400px] max-md:left-1
+                    max-sm:w-[300px] max-sm:left-[300px]'>
+                    <div className='w-[55px] h-[55px] rounded-full bg-orange absolute right-[83px] top-[-25px] z-30 border border-gray-600/10'>
+                      <img src={SImg} alt="" className='z-40 px-2 py-2'/>
+                    </div>
+                    <div className='w-[180px] h-full mr-5 bg-white rounded-2xl shadow-2xl z-20
+                    hover:shadow-xl hover:shadow-orange transition-shadow dark:bg-gray-700/70 dark:hover:bg-orange dark:hover:shadow-md dark:hover:transition-colors'>
+
+                                  <h1 className='text-lg text-center py-12 text-gray-800/90 font-semibold dark:text-white'> 5۰+  مدرس</h1>
+                    </div>
+
+                </div>
+
+
           </div>
-
-
-     </motion.div>
-
-     <div className="flex justify-center items-center">
-            <motion.img
-              initial={{opacity:0 , x:200}}
-              animate={{opacity:1 , x:0}}
-              transition={{type:"spring" , stiffness:100 , delay:0.2}}
-  
-              src={HeroBImg}
-              alt=""
-              className="w-[400px] md:w-[600px] absolute left-24 top-[270px]
-              xl:w-[550px] max-xl:left-[0px] max-md:left-[50px] max-md:top-[180px] max-md:w-[500px]"
-            />
-          </div>
-
-          <div className='flex justify-end  w-full h-[100px] absolute bottom-5 left-3 max-xl:mb-[30px] max-md:top-[400px] max-md:left-1
-            max-sm:w-[300px] max-sm:left-[300px]'>
-            <div className='w-[55px] h-[55px] rounded-full bg-orange absolute right-[83px] top-[-25px] z-30 border border-gray-600/10'>
-              <img src={SImg} alt="" className='z-40 px-2 py-2'/>
-            </div>
-            <div className='w-[180px] h-full mr-5 bg-white rounded-2xl shadow-2xl z-20
-            hover:shadow-xl hover:shadow-orange transition-shadow dark:bg-gray-700/70 dark:hover:bg-orange dark:hover:shadow-md dark:hover:transition-colors'>
-
-                          <h1 className='text-lg text-center py-12 text-gray-800/90 font-semibold dark:text-white'> 5۰+  مدرس</h1>
-            </div>
-
-        </div>
-
-
-    </div>
 
           {/* brand section  */}
           <div
             className="flex flex-col justify-center py-14
-          md:pr-16 xl:pr-40 md:py-0"
+            md:pr-16 xl:pr-40 md:py-0"
           >
             <div className="text-center md:text-left space-y-6 relative">
 
@@ -111,25 +112,25 @@ const Hero = () => {
                
               </div>
           
-            <motion.h1
-             variants={SliderRight(0.4)}
-             initial="hidden"
-             animate="visible"
-              className="text-5xl font-semibold lg:text-6xl
-              !leading-tight
-              text-right max-xl:ml-[150px] max-md:text-center max-md:ml-[10px]"
-            >
-              عنوان  <span className="text-primary max-xl:mr-[150px] max-md:mr-[10px]">آموزشگاه</span>
-            </motion.h1>
-            <motion.p
-               variants={SliderRight(0.6)}
-               initial="hidden"
-               animate="visible"
-               className='text-right font-semibold text-gray-600 max-xl:ml-[150px] max-md:ml-[20px] max-md:text-center dark:text-white'
-            >
-             مرجع اموزش زنده و تعاملی برنامه نویسی حوزه وب 
-             با دسترسی به بیش از هفت هزار ویدیوی اموزشی به زبان فارسی
-            </motion.p>
+              <motion.h1
+              variants={SliderRight(0.4)}
+              initial="hidden"
+              animate="visible"
+                className="text-5xl font-semibold lg:text-6xl 
+                !leading-tight
+                text-right max-xl:ml-[150px] max-md:text-center max-md:ml-[10px]"
+              >
+                عنوان  <span className="text-primary max-xl:mr-[150px] max-md:mr-[10px]">آموزشگاه</span>
+              </motion.h1>
+              <motion.p
+                variants={SliderRight(0.6)}
+                initial="hidden"
+                animate="visible"
+                className='text-right font-semibold text-gray-600 max-xl:ml-[150px] max-md:ml-[20px] max-md:text-center dark:text-white'
+              >
+              مرجع اموزش زنده و تعاملی برنامه نویسی حوزه وب 
+              با دسترسی به بیش از هفت هزار ویدیوی اموزشی به زبان فارسی
+              </motion.p>
             </div>
             {/* button section  */}
             <motion.div
@@ -139,13 +140,13 @@ const Hero = () => {
               className="flex gap-8 justify-center md:justify-start 
               !mt-8 items-center relative max-lg:ml-[50px] max-lg:w-[300px] max-md:ml-[100px] "
             >
-              <div className='w-10 h-10 rounded-full bg-orange absolute left-[140px] z-50 -top-[15px] max-xl:left-[100px] max-lg:left-[80px]'>
+              {/* <div className='w-10 h-10 rounded-full bg-orange absolute left-[140px] z-50 -top-[15px] max-xl:left-[100px] max-lg:left-[80px]'>
                 <img src={SearchImg} alt="" className='px-1.5 py-1.5'/>
                 </div>  
                 <input placeholder='... دنبال چی میگردی ؟' type="search" className='w-3/4 h-[50px]  absolute right-0 -top-5 rounded-3xl shadow-sm shadow-gray-400  text-right font-semibold
                 text-gray-600 text-sm outline-none indent-1 hover:shadow-xl hover:shadow-orange transition-shadow dark:bg-gray-700/70  dark:hover:bg-orange dark:hover:shadow-md dark:hover:transition-colors' />
-              
-              
+               */}
+              <SearchBar placeholder={'... دنبال چی میگردی ؟'}/>
              
             </motion.div>
           </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -49,71 +49,75 @@ const Navbar = () => {
     initial={{opacity:0}}
     animate={{opacity:1}}
     transition={{duration:0.5 , delay:0.5}}
->
+    >
 
-<div>
-<div className='container flex justify-between items-center py-6 dark:bg-gray-900  duration-200 '>
- {/* ACCOUNT section  */}
- <div className='text-2xl flex items-center gap-2 font-bold'>
-    <div className='relative w-36 max-md:w-[150px]  max-sm:w-[150px] max-lg:w-[150px] max-xl:w-[200px]  h-10 bg-primary
-     mt-8 rounded-3xl hover:shadow-lg hover:shadow-orange dark:bg-orange  dark:hover:shadow-lg dark:hover:shadow-orange
-    transition-shadow'>
-     <img src={UserImg} alt="" className='w-7 h-7  absolute top-1.5 left-4'/>
-     <h1 className='text-sm  font-semibold text-white text-right leading-loose px-2 py-1 '>حساب کاربری</h1>
-  </div>  
-  <div className='flex justify-center flex-row gap-4 ml-4'>
-      <img src={ShopImg} alt="" className='mt-8'/>
-      <div className='relative'>
-   <img src={FavImg} alt=""  className='mt-8 max-xl:mt-[35px] max-xl:w-[80px]  max-lg:w-[30px] max-2xl:mt-[35px] max-2xl:w-[40px]'/>
-    <div className='w-4 h-4 bg-orange rounded-full absolute bottom-0 right-10'></div>
+        <div>
+            <div className='container flex justify-between items-center py-6 dark:bg-gray-900  duration-200'>
+                {/* ACCOUNT section  */}
+                <div className='text-2xl flex items-center gap-1 font-bold '>
+                    <div className='relative w-36 max-md:w-[150px]  max-sm:w-[150px] max-lg:w-[150px] max-xl:w-[150px] bg-primary
+                    mt-8 rounded-3xl hover:shadow-lg hover:shadow-orange dark:bg-orange  dark:hover:shadow-lg dark:hover:shadow-orange
+                    transition-shadow px-4 py-[7px]'>
+                        <img src={UserImg} alt="" className='w-6 h-6  absolute '/>
+                        <h1 className='text-sm  font-semibold text-white text-right leading-loose mr-1 pb-1 '>حساب کاربری</h1>
+                    </div> 
 
-      </div>
+                    <div className='flex justify-center flex-row gap-2 ml-3'>
+                        <div className='relative '>
+                            <img src={ShopImg} alt="" className='mt-9'/>
+                            <div className='w-3 h-3 bg-orange rounded-full absolute bottom-[1px] right-0 '></div>
+                        </div>
+                        <img src={FavImg} alt=""  className='mt-8 
+                        max-lg:w-[30px] 
+                        max-xl:mt-[35px] max-xl:w-[30px]  
+                        max-2xl:mt-[35px] max-2xl:w-[30px]'/>
 
-          {/* darkmode switch */}
-        <div className='mt-[34px] max-lg:mt-[35px]'>
-            <DarkMode/>
+                        {/* darkmode switch */}
+                        <div className='mt-[40px] max-lg:mt-[35px]'>
+                           <DarkMode/>
+                        </div>
+                    </div>                  
+                    
+                </div>
+
+                {/* menu section  */}
+                <div className='hidden lg:block'>
+                    <ul className='flex items-center gap-4 pr-10 mx-auto'>
+                        {
+                            NavbarMenu.map((item)=>(
+                                <li key={item.id}>
+                                    <a href={item.link}
+                                        className='inline-block justify-center text-gray-500
+                                        text-lg max-xl:text-base max-xl:text-right whitespace-nowrap  py-1 px-2 xl:px-3
+                                        hover:text-secondary transition-all duration-300 
+                                        font-medium  mt-8 dark:text-white  dark:hover:text-orange'
+                                    >{item.title}</a>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
+
+                {/* Logo section  */}
+                <div className='hidden lg:block space-x-6'>
+                <div className=' py-6 mt-0'>
+                {/* <img src={Logo} alt="" className='rounded-full shadow-xl shadow-gray-600'/> */}
+                    </div> 
+                
+                </div>
+
+                {/* mobile hamburger section  */}
+                <div className={`${isOpen? "rotate-90" : "rotate-0"} transition duration-500 lg:hidden`} onClick={()=>setIsOpen(!isOpen)}>
+                            <MdMenu className='cursor-pointer text-4xl'/>
+                </div>
+            
+            </div>
         </div>
-  </div>
-    
-    
- </div>
- {/* menu section  */}
- <div className='hidden lg:block'>
-    <ul className='flex items-center gap-4 pr-10 mx-auto'>
-        {
-            NavbarMenu.map((item)=>(
-                <li key={item.id}>
-                    <a href={item.link}
-                        className='inline-block justify-center text-gray-500
-                         text-lg max-xl:text-base max-xl:text-right whitespace-nowrap  py-1 px-2 xl:px-3
-                         hover:text-secondary transition-all duration-300 
-                         font-medium  mt-8 dark:text-white  dark:hover:text-orange'
-                    >{item.title}</a>
-                </li>
-            ))
-        }
-    </ul>
- </div>
- {/* Logo section  */}
- <div className='hidden lg:block space-x-6'>
-   <div className=' py-6 mt-0'>
-   {/* <img src={Logo} alt="" className='rounded-full shadow-xl shadow-gray-600'/> */}
-    </div> 
-   
- </div>
 
-  {/* mobile hamburger section  */}
-  <div className='lg:hidden' onClick={()=>setIsOpen(!isOpen)}>
-            <MdMenu className='text-4xl '/>
-         </div>
- 
-</div>
-</div>
+        {/* mobile sidebar section  */}
+        <ResponsiveMenu isOpen={isOpen}/>
 
-  {/* mobile sidebar section  */}
-  <ResponsiveMenu isOpen={isOpen}/>
-
-</motion.div>
+    </motion.div>
   )
 }
 
