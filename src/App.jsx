5067081e954dd574421f1b765910df71fc19cb19
sara <saramoslemi29@gmail.com>
2024-10-07 21:56:49 +0330

    '

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,15 @@ import { Outlet } from "react-router-dom"
 import Footer from "./components/Footer"
 import Navbar from "./components/Navbar"
 
-<<<<<<< HEAD
-=======
 
->>>>>>> main
 
 function App() {
 
 
   return (
-<<<<<<< HEAD
- <></>
 
-=======
+ 
+
     <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
     <Navbar/>
 
@@ -23,7 +19,7 @@ function App() {
 
     <Footer/>
     </div>
->>>>>>> main
+
   )
 }
 
