453b44637084de38865f7ec50a0ca2f69fba5c1b
Aria <Aria.Blackpurple@gmail.com>
2024-10-07 23:40:46 +0330

    routes new

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
  
 
-    <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
+    <div>
     <Navbar/>
 
       <Outlet/>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,7 @@ import Logo from "../assets/landing/logo1.svg";
 import ResponsiveMenu from './ResponsiveMenu';
 import { MdDarkMode, MdMenu } from 'react-icons/md';
 import DarkMode from './DarkMode';
+import { NavLink } from 'react-router-dom';
 
 
 
@@ -15,12 +16,12 @@ export const NavbarMenu =[
   {
       id:1,
       title:"تماس با ما",
-      link:"/",
+      link:"#",
   },
   {
       id:2,
       title:"اخبار و مقالات",
-      link:"#",
+      link:"/article-news",
   },
   {
       id:3,
@@ -30,12 +31,12 @@ export const NavbarMenu =[
   {
       id:4,
       title:"دوره ها",
-      link:"#",
+      link:"/courses",
   },
   {
       id:5,
       title:"صفحه نخست",
-      link:"#",
+      link:"/",
   },
 ]
 
@@ -55,12 +56,12 @@ const Navbar = () => {
 <div className='container flex justify-between items-center py-6 dark:bg-gray-900  duration-200 '>
  {/* ACCOUNT section  */}
  <div className='text-2xl flex items-center gap-2 font-bold'>
-    <div className='relative w-36 max-md:w-[150px]  max-sm:w-[150px] max-lg:w-[150px] max-xl:w-[200px]  h-10 bg-primary
+    <NavLink to={"/auth"} className='relative w-36 max-md:w-[150px]  max-sm:w-[150px] max-lg:w-[150px] max-xl:w-[200px]  h-10 bg-primary
      mt-8 rounded-3xl hover:shadow-lg hover:shadow-orange dark:bg-orange  dark:hover:shadow-lg dark:hover:shadow-orange
     transition-shadow'>
      <img src={UserImg} alt="" className='w-7 h-7  absolute top-1.5 left-4'/>
      <h1 className='text-sm  font-semibold text-white text-right leading-loose px-2 py-1 '>حساب کاربری</h1>
-  </div>  
+  </NavLink>  
   <div className='flex justify-center flex-row gap-4 ml-4'>
       <img src={ShopImg} alt="" className='mt-8'/>
       <div className='relative'>
