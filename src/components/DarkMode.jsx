534d05c8dfcfaadcf887259a94443bb123c10e78
fatemeh_bkhshian <flowerfbf18@gmail.com>
2024-10-24 21:05:45 +0330

    themeIsDone

--- src/components/DarkMode.jsx
+++ src/components/DarkMode.jsx
@@ -4,11 +4,11 @@ import React, { useEffect, useState } from 'react'
 import  light from "../assets/darkmode/icons8-moon-80(1).png"
 import first from "../assets/darkmode/first.svg"
 import second from "../assets/darkmode/second.svg"
+import third from "../assets/darkmode/third.svg"
 
 
 const DarkMode = () => {
 
-    const [themeImage , setthemeImage] = useState(light)
     const [theme , setTheme] = useState(
         localStorage.getItem("theme")?localStorage.getItem
         ("theme"):"maintheme"
@@ -18,71 +18,70 @@ const DarkMode = () => {
 
 
     useEffect(()=>{
-        if(theme === "dark"){
-            element.classList.remove("maintheme")
-            element.classList.remove("secondTheme")
+        if(theme === "dark"){            
             element.classList.add("dark")
             localStorage.setItem("theme","dark")
         }
         else if(theme === "maintheme"){
             element.classList.remove("dark")
             element.classList.remove("secondTheme")
+            element.classList.remove("thirdTheme")
             element.classList.add("maintheme")
             localStorage.setItem("theme","maintheme")
         }
         else if(theme === "secondTheme"){
+            element.classList.remove("thirdTheme")
             element.classList.remove("maintheme")
             element.classList.remove("dark")
             element.classList.add("secondTheme")
             localStorage.setItem("theme","secondTheme")
         }
-
+        else if(theme === "thirdTheme"){
+            element.classList.remove("secondTheme")
+            element.classList.remove("maintheme")
+            element.classList.remove("dark")
+            element.classList.add("thirdTheme")
+            localStorage.setItem("theme","thirdTheme")
+        }
     },[theme])
   
 
   return (
     <div 
     className='group cursor-pointer ml-[90%] dark:bg-orange rounded-full flex items-center 
-        z-[9999] w-[55px] h-[55px] bg-green fixed bottom-8 right-14 hover:w-80 transition duration-500
-        max-lg:top-6 max-lg:left-6  
-        max-md:bottom-8
-        max-sm:bottom-8     '>
+        z-[9999] w-[55px] h-[55px] bg-green fixed bottom-8 right-12 hover:w-80 transition-all duration-700
+            '>
     <div >
-        <img src={themeImage}
+        <img src={`${theme === "dark" ? light : theme === "maintheme" ? first : second}`}
         
         className= "absolute top-2 right-2 w-[40px] h-[40px] transition-all duration-200  "/>  
        
  
     </div>
     <div className='flex gap-3 ml-5'>
-        <div className='' onClick={()=>setTheme("dark")} >
+        <div className='' onClick={()=>setTheme(`${theme === 'dark'? "maintheme" : "dark"}`)} >
         <img src={light} 
-            onClick={()=>setthemeImage(light)}
-            className="w-[40px] h-[40px]  transition-all duration-200 hidden group-hover:block" 
+            className="w-[40px] h-[40px]  transition-all duration-200  group-hover:block" 
             
         />
         </div>
         <div className='' onClick={()=>setTheme("maintheme")} >
         <img src={first} 
-            onClick={()=>setthemeImage(first)}
-            className="w-[40px] h-[40px]  transition-all duration-200 hidden group-hover:block" 
+            className="w-[40px] h-[40px]  transition-all duration-200  group-hover:block" 
             
         />
         </div>
         <div className='' onClick={()=>setTheme("secondTheme")} >
         <img src={second} 
-            onClick={()=>setthemeImage(second)}
-            className="w-[40px] h-[40px]  transition-all duration-200 hidden group-hover:block" 
-            
-        />
-        </div>
-        {/* <div className='' onClick={()=>setTheme(theme === "light" ? "dark" : "light")} >
-        <img src={first} 
-            onClick={()=>setthemeImage(first)}
             className="w-[40px] h-[40px]  transition-all duration-200  group-hover:block" 
             
         />
-        </div> */}
+        </div>
+        <div className='' onClick={()=>setTheme("thirdTheme")} >
+        <img src={third} 
+            className="w-[40px] h-[40px]  transition-all duration-200  group-hover:block"             
+        />
+        </div>
     </div>
     
     </div>
